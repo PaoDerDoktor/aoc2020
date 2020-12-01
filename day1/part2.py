cd527acaf748836184f794d75db9764ba6d2a6e0
PaoDerDoktor <PaoDerDoktor@gmail.com>
2020-12-01 12:26:02 +0100

    Did day 1 !

---
+++ day1/part2.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+with open('day1/input.txt', 'r') as inFile:
+    data: List[int] = [int(line) for line in inFile.readlines()]
+    
+    i: int = 0
+    j: int = 0
+    k: int = 0
+    
+    found: bool = False
+    for i in range(len(data)):
+        for j in range(len(data)):
+            for k in range(len(data)):
+                if data[i]+data[j]+data[k] == 2020:
+                    found = True
+                    break
+            if found:
+                break
+        if found:
+            break
+        
+    print(data[i]*data[j]*data[k])
