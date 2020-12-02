5e88dbbe4fa49e58170ad92b4c9e66ddbda0dd05
PaoDerDoktor <PaoDerDoktor@gmail.com>
2020-12-02 12:37:43 +0100

    Did day 2

---
+++ day2/part1.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple
+
+with open('day2/input.txt', 'r') as inputFile:
+    data: List[Tuple[Tuple[int, int], str, str]] = [((int(line.split(': ')[0].split(' ')[0].split('-')[0]),
+                                                      int(line.split(': ')[0].split(' ')[0].split('-')[1])),
+                                                     line.split(': ')[0].split(' ')[1],
+                                                     line.split(':')[1].strip()) for line in inputFile.readlines()]
+    
+    valids: int = 0
+    for entry in data:
+        if entry[0][0] <= entry[2].count(entry[1]) <= entry[0][1]:
+            valids += 1
+    
+    print(valids)
